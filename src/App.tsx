d8cd78d4ae41eac159af3f3e7ae7c54e9de1cae1
EphraimHaber <ephraimxyz@gmail.com>
2025-05-06 01:02:37 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import { TreeView, type TreeDataItem } from '@/components/tree-view';
+
+const data: TreeDataItem[] = [
+  {
+    id: '1',
+    name: 'Item 1',
+    children: [
+      {
+        id: '2',
+        name: 'Item 1.1',
+        children: [
+          {
+            id: '3',
+            name: 'Item 1.1.1',
+          },
+          {
+            id: '4',
+            name: 'Item 1.1.2',
+          },
+        ],
+      },
+      {
+        id: '5',
+        name: 'Item 1.2',
+      },
+    ],
+  },
+  {
+    id: '6',
+    name: 'Item 2',
+    children: [
+      {
+        id: '7',
+        name: 'Item 2.1',
+      },
+      {
+        id: '8',
+        name: 'Item 2.2',
+        children: [
+          {
+            id: '9',
+            name: 'Item 2.2.1',
+          },
+          {
+            id: '10',
+            name: 'Item 2.2.2',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const App: React.FC = () => {
+  const [selected, setSelected] = useState<string>('');
+  return (
+    <>
+      <TreeView data={data} onSelectChange={(item) => setSelected(item?.name || '')} />
+      <h3>Selected: {selected || 'Nothing'}</h3>
+    </>
+  );
+};
+
+export default App;
